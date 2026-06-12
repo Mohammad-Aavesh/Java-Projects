17179aacd7694c14cc1beec920089af6190f8498
Mohammad Aavesh <mohammadaavesh11@gmail.com>
2026-06-12 23:21:14 +0530

    Delete 0.1 directory

--- 0.1/Transactions.sql
+++
@@ -1,8 +0,0 @@
-create database RECORDS;
-use RECORDS;
-create table transactions(Date varchar(15),Type varchar(10),Amount int,Descrip varchar(50));
-select * from transactions where Amount = 4000;
-use RECORDS;
-alter table students add id INT AUTO_INCREMENT PRIMARY KEY;
-select * from students;
-Insert into students (Full_Name,Marks,City) values ("bhavyansh",67,"Harda");
